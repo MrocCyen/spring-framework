cecdfbdd745a54e354ff914d346e47a85f265046
qingshanpeng <qingshanpeng@126.com>
2021-07-27 14:35:09 +0800

    proxy execute chain test

---
+++ spring-test/src/main/java/org/springframework/qingsp/aop_ExecuteChain/A.java
@@ -0,0 +1,9 @@
+package org.springframework.qingsp.aop_ExecuteChain;
+
+/**
+ * @author qingshanpeng
+ * @date 2021/7/27 14:30
+ * @since 标果工厂-脱骨李
+ */
+public class A {
+}
---
+++ spring-test/src/main/java/org/springframework/qingsp/aop_ExecuteChain/MethodInvocation.java
@@ -0,0 +1,10 @@
+package org.springframework.qingsp.aop_ExecuteChain;
+
+/**
+ * @author qingshanpeng
+ * @date 2021/7/27 14:18
+ * @since 标果工厂-脱骨李
+ */
+public interface MethodInvocation {
+	Object process();
+}
