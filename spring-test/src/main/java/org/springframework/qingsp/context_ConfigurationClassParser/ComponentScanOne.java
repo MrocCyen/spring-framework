902052b9dee065b813e92033742e7f966aecf821
qingshanpeng <qingshanpeng@126.com>
2022-02-21 14:37:42 +0800

    ConfigurationClassParser

---
+++ spring-test/src/main/java/org/springframework/qingsp/context_ConfigurationClassParser/ComponentScanOne.java
@@ -0,0 +1,14 @@
+package org.springframework.qingsp.context_ConfigurationClassParser;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author qingshanpeng
+ * @date 2022/2/21 14:04
+ * @since 标果工厂
+ */
+@Component
+@ComponentScan("org.springframework.qingsp.context_ConfigurationClassParser_ComponentScan")
+public class ComponentScanOne {
+}
---
+++ spring-test/src/main/java/org/springframework/qingsp/context_ConfigurationClassParser_ComponentScan/ComponentScanThree.java
@@ -0,0 +1,12 @@
+package org.springframework.qingsp.context_ConfigurationClassParser_ComponentScan;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author qingshanpeng
+ * @date 2022/2/21 14:04
+ * @since 标果工厂
+ */
+@Component
+public class ComponentScanThree {
+}
